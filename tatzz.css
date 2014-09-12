3a9b0b9fd3c9c1fca075465b6643142784eac241
Tathagata Banerjee <tatha04banerjee@gmail.com>
2014-09-12 18:22:35 +0530

    css file
    upload

---
+++ tatzz.css
@@ -0,0 +1,50 @@
+body{
+	background-color:#d4dde4;
+}
+#clientsDropDown #clientsDashboard {
+display: none;  
+}
+#clientsDropDown #clientsDashboard  {
+	background-color:#414142;
+	}
+/* CUSTOMIZE THE CAROUSEL
+-------------------------------------------------- */
+
+/* Carousel base class */
+.carousel {
+  height: 500px;
+  margin-bottom: 60px;
+  }
+  
+/* Since positioning the image, we need to help out the caption */
+.carousel-caption {
+  z-index:0;
+	color:#428bca
+}
+
+/* Declare heights because of positioning of img element */
+.carousel .item {
+  height: 500px;
+	
+  background-color: #777777;
+	
+}
+.carousel-inner > .item > img {
+      position:absolute;
+	right:375;
+	height: 500px;
+	width:500px;
+	min-width:50%;
+	}
+
+.carousel-indicators{
+	text-align:right
+}
+@media (min-width: 768px) {
+  /* Bump up size of carousel content */
+  .carousel-caption p {
+    margin-bottom: 20px;
+    font-size: 21px;
+    line-height: 1.4;	 
+	  }
+ 
